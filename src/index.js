87ec2759b552f8669d66252bfcf2ee5e0e0234d9
lampv4 <v.lampv4@vinsmart.net.vn>
2021-02-06 20:49:56 +0700

    add nornalize & reset style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style/normalize.scss';
+import './style/reset.scss';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
