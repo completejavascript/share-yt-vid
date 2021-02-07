c934868bbb7765521af20ed7eed33047706c8a6b
lampv4 <v.lampv4@vinsmart.net.vn>
2021-02-07 22:07:28 +0700

    navigate to home or refresh when click title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { useAuthContext } from '../../provider/Auth';
 import Login from '../Login/Login';
 import UserAction from '../UserAction/UserAction';
+import { PATH_HOME } from '../../utils/constants';
 import './Header.scss';
 
 const Header = () => {
   const { currentUser } = useAuthContext();
+
+  const handleTitleClick = () => {
+    window.location.pathname = PATH_HOME;
+  };
+
   return (
     <header className="header-container">
-      <h1 className="title">Funny Movies</h1>
+      <h1 className="title" onClick={handleTitleClick}>
+        Funny Movies
+      </h1>
       {currentUser ? <UserAction email={currentUser.email} /> : <Login />}
     </header>
   );
