2d5eb8c209f92ef40a82786adba33608f006efad
lampv4 <v.lampv4@vinsmart.net.vn>
2021-02-07 22:04:21 +0700

    Use other method to add movie to prevent duplicate

--- src/firebase/firebaseStore.js
+++ src/firebase/firebaseStore.js
@@ -4,15 +4,19 @@ import firebaseApp from './firebaseApp';
 const MOVIE_COLLECTION = 'movies';
 
 export const addMovie = ({ video_id, title, description }) => {
-  return firebaseApp.firestore().collection(MOVIE_COLLECTION).add({
-    video_id,
-    title: title,
-    shared_by: firebaseApp.auth().currentUser.email,
-    description: description,
-    created_date: firebase.firestore.FieldValue.serverTimestamp(),
-    updated_date: firebase.firestore.FieldValue.serverTimestamp(),
-    voted_date: 0,
-    upvoded_users: [],
-    downvoted_users: [],
-  });
+  return firebaseApp
+    .firestore()
+    .collection(MOVIE_COLLECTION)
+    .doc(video_id)
+    .set({
+      video_id,
+      title: title,
+      shared_by: firebaseApp.auth().currentUser.email,
+      description: description,
+      created_date: firebase.firestore.FieldValue.serverTimestamp(),
+      updated_date: firebase.firestore.FieldValue.serverTimestamp(),
+      voted_date: 0,
+      upvoded_users: [],
+      downvoted_users: [],
+    });
 };
